f8afb9a5cac25a56edeb44bf9a6941b1229ae807
Ayush Agrawal <ayush.jan10@gmail.com>
2021-01-30 12:39:26 +0530

    Add three new collections

--- test.py
+++ test.py
@@ -3,24 +3,48 @@ from discord.ext import commands
 from keyconfig import *
 import pymongo
 from datetime import datetime
+import schedule
+import validators
 
 bot = commands.Bot(command_prefix = '$')
 myclient = pymongo.MongoClient(MONGO_URL)
 db = myclient["linkbot"]
-coll = db["bot"]
+
+collist = db.list_collection_names()
+collections = ["schedules", "users", "courses"]
+
+for collect in collections:
+    if collect not in collist:
+        db.create_collection(collect)
+
+schedules = db["schedules"]
+users = db["users"]
+courses = db["courses"]
+
 class Schedule():
 
-    def __init__(self, subject, time, section, link):
-        
-        schedule_dict = {"subject": subject, "time": time, "section": section, "link": [link]}
-        coll.insert_one(schedule_dict)
+    def __init__(self, subject, day, time, section, link):
+        if validators.url(link):
+            course = courses.find_one_and_update({"name": subject}, {"$set": {"name": subject}}, upsert= True, return_document = pymongo.ReturnDocument.AFTER)
+            print(course)
+            sched = schedules.find_one({"subject": course['_id'], "section": section})
+            if sched is None:
+                schedules.insert_one({"subject": course['_id'], "day": day, "time": time, "section": section, "link": [link]})
+            else:
+                schedules.update_one({"query": sched, "$push": {"link": link}})
+        else:
+            raise Exception('Invalid URL Format.')
     
     #adds meet link corresponding to a course
     @staticmethod
     async def add_link(ctx, *args):
-        query = {"subject": args[0], "section": args[1]}
-        coll.update_one(query, {"$push": {"link": args[2]}})
-        await ctx.send(f'Link added (if course exists) to {args[0]} {args[1]}')
+        if validators.url(args[2]):
+            course = courses.find_one_and_update({"name": subject}, {"$set": {"name": subject}}, upsert= True, return_document = pymongo.ReturnDocument.AFTER)
+            query = {"subject": course['_id'], "section": args[1]}
+            schedules.update_one(query, {"$push": {"link": args[2]}})
+            ctx.send(f'Link added to {args[0]} {args[1]}')
+        else:
+            ctx.send(f'Incorrect URL Format.')
 
     #retrieving link(s) of a subject from the db
     @staticmethod
@@ -49,11 +73,14 @@ async def ping(ctx):
 # Registers the course in the database
 @bot.command(aliases=['register', 'add'])
 async def register_course(ctx, *args):
-    if len(args) != 4:
-        await ctx.send('Usage: Course, time, section, link')
+    if len(args) != 5:
+        await ctx.send('Usage: Course, day, time, section, link')
     else:
-        Schedule(args[0], args[1], args[2], args[3])
-        await ctx.send(f'{args[0]} registered.')
+        try:
+            Schedule(args[0], args[1], args[2], args[3], args[4])
+            await ctx.send(f'{args[0]} registered.')
+        except Exception as e:
+            await ctx.send(f'{e}')
 
 @bot.command()
 async def deregister(ctx, *args):
