16bfb70644ad83dcdadcaa5b906784015b41cc77
Ayush Agrawal <ayush.jan10@gmail.com>
2021-01-30 17:23:35 +0530

    Changed return for init func

--- test.py
+++ test.py
@@ -23,19 +23,27 @@ courses = db["courses"]
 
 class Schedule():
 
+    @staticmethod
+    def get_course(name):
+        return courses.find_one({"name": name})
+
+    @staticmethod
+    def get_schedule(name, section):
+        return schedules.find_one({"course": Schedule.get_course(name)['_id'], "section": section})
+
     def __init__(self, subject, day, time, section, link):
         if validators.url(link):
             course = courses.find_one_and_update({"name": subject}, {"$set": {"name": subject}}, upsert= True, return_document = pymongo.ReturnDocument.AFTER)
             sched = schedules.find_one({"course": course['_id'], "section": section})
             if sched is None:
                 schedules.insert_one({"course": course['_id'], "day": day.lower(), "time": time, "section": section, "link": [link]})
-                return 1
             else:
                 if link in sched['link']:
-                    return 2
+                    raise Exception("Link already added!")
                 else:
                     schedules.update_one({"query": sched, "$push": {"link": link}})
-        return 0
+        else:
+            raise Exception("Invalid URL format.")
     
     #adds meet link corresponding to a course
     @staticmethod
@@ -62,14 +70,14 @@ class Schedule():
 >>>>>>> remove all method added
     @staticmethod
     def get_link(*args):
-        sched = get_schedule(args[0], args[1])
+        sched = Schedule.get_schedule(args[0], args[1])
         if sched:
             return sched['link'], 1
         return None, 0
     
     @staticmethod
     def deregister(*args):
-        if get_course(args[0]):
+        if Schedule.get_course(args[0]):
             course = courses.delete_one({"name": args[0]})
             return 1
         else:
@@ -77,20 +85,12 @@ class Schedule():
 
     @staticmethod
     def remove_link(*args):
-        sched = get_schedule(args[0], args[1])
+        sched = Schedule.get_schedule(args[0], args[1])
         if args[2] in sched['link']:
             sched['link'].remove(args[2])
             schedules.update_one(sched, {"$set": {"link": sched['link']}})   
             return 1
         return 0
-
-    @staticmethod
-    def get_course(name):
-        return courses.find_one({"name": name})
-
-    @staticmethod
-    def get_schedule(name, section):
-        return schedules.find_one({"name": get_course()['_id'], "section": section})
 
 
     @staticmethod
@@ -112,13 +112,11 @@ async def register_course(ctx, *args):
     if len(args) != 5:
         await ctx.send('Usage: Course, day, time, section, link')
     else:
-        status = Schedule(args[0], args[1], args[2], args[3], args[4])
-        if status == 1:
-            await ctx.send(f'{args[0]} {args[1]} registered.')
-        elif status == 2:
-            await ctx.send(f'Link already exists!')
-        elif status == 0:
-            await ctx.send('Invalid URL Format.')
+        try:
+            Schedule(args[0], args[1], args[2], args[3], args[4])
+            await ctx.send(f"{args[0]} {args[1]} registered! :grin:")
+        except Exception as e:
+            await ctx.send(f'{e}')
 
 @bot.command()
 async def deregister(ctx, *args):
@@ -144,7 +142,10 @@ async def getlink(ctx, *args):
     links, status = Schedule.get_link(*args)
     if status:
         if len(links):
-            await ctx.send('\n'.join(links))
+            temp_links = []
+            for link in links:
+                temp_links.append("<" + link + ">")
+            await ctx.send('\n'.join(temp_links))
         else:
             await ctx.send("No links added :pensive:")
     else:
